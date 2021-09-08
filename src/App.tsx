3866cdf4cb30c8641e6f54f0889061de857e647e
Stephen Scholtz <scholtz.gnome@gmail.com>
2021-09-08 12:18:07 +0200

    Put beginner 8kyu challenges in

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,430 @@ function App() {
   return (
     <div className="App">
       <header>Welcome to Codewars Journey</header>
+      <div>
+        <ol>
+          <li>
+            <a
+              href="https://www.codewars.com/kata/50654ddff44f800200000004"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Multiply
+            </a>
+            <a
+              href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/return"
+              target="_blank"
+              rel="noreferrer"
+            >
+              - HINT
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.codewars.com/kata/511f0fe64ae8683297000001"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Basic Training: Add item to an Array
+            </a>
+            <a
+              href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/push"
+              target="_blank"
+              rel="noreferrer"
+            >
+              - HINT
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.codewars.com/kata/523b4ff7adca849afe000035"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Function 1 - Hello World
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.codewars.com/kata/523b623152af8a30c6000027"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Function 2 - Squaring An Argument
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.codewars.com/kata/50ee6b0bdeab583673000025"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Basic Variable Assignment
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.codewars.com/kata/523b66342d0c301ae400003b"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Function 3 - Multiplying Two Numbers
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.codewars.com/kata/56200d610758762fb0000002"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Grasshopper - Basic Function Fixer
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.codewars.com/kata/53ee5429ba190077850011d4"
+              target="_blank"
+              rel="noreferrer"
+            >
+              You Can't Code Under Pressure #1
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.codewars.com/kata/55a70521798b14d4750000a4"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Returning Strings
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.codewars.com/kata/5612e743cab69fec6d000077"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Grasshopper - Variable Assignment Debug
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.codewars.com/kata/56dae9dc54c0acd29d00109a"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Grasshopper - Function Syntax Debugging
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.codewars.com/kata/5625618b1fe21ab49f00001f"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Grasshopper - Debug sayHello()
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.codewars.com/kata/55ca77fa094a2af31f00002a"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Grasshopper - Messi Goals
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.codewars.com/kata/55f73be6e12baaa5900000d4"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Grasshopper - Messi Goals Function
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.codewars.com/kata/5ab6538b379d20ad880000ab"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Area or Perimeter
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.codewars.com/kata/55f9b48403f6b87a7c0000bd"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Beginner Series #1 - School Paperwork
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.codewars.com/kata/55f9bca8ecaa9eac7100004a"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Beginner Series #2 - Clock
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.codewars.com/kata/55fab1ffda3e2e44f00000c6"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Beginner Series #4 - Cockroach
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.codewars.com/kata/582cb0224e56e068d800003c"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Keep Hydrated!
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.codewars.com/kata/57089707fe2d01529f00024a"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Grasshopper - If/else Syntax Debug
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.codewars.com/kata/5772da22b89313a4d50012f7"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Grasshopper - Personalized Message
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.codewars.com/kata/563a631f7cbbc236cf0000c2"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Grasshopper - Terminal Game Move Function
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.codewars.com/kata/586c1cf4b98de0399300001d"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Grasshopper - Terminal Game Combat Function
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.codewars.com/kata/551b4501ac0447318f0009cd"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Convert a Boolean to a String
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.codewars.com/kata/51f9d93b4095e0a7200001b8"
+              target="_blank"
+              rel="noreferrer"
+            >
+              How Many Lightsabers Do You Own?
+            </a>
+            <a
+              href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Conditional_Operator"
+              target="_blank"
+              rel="noreferrer"
+            >
+              - HINT
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.codewars.com/kata/53af2b8861023f1d88000832"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Are You Playing Banjo?
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.codewars.com/kata/56aed32a154d33a1f3000018"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Fix the Bugs (Syntax) - My First Kata
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.codewars.com/kata/515e188a311df01cba000003"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Get Planet Name By ID
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.codewars.com/kata/555a67db74814aa4ee0001b5"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Is It Even?
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.codewars.com/kata/5861d28f124b35723e00005e"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Will You Make It?
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.codewars.com/kata/53934feec44762736c00044b"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Number toString
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.codewars.com/kata/50654ddff44f800200000007"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Short Long Short
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.codewars.com/kata/59ca8246d751df55cc00014c"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Is He Gonna Survive?
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.codewars.com/kata/57d814e4950d8489720008db"
+              target="_blank"
+              rel="noreferrer"
+            >
+              N-th Power
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.codewars.com/kata/56dec885c54a926dcd001095"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Opposite Number
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.codewars.com/kata/55cbc3586671f6aa070000fb"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Grasshopper - Check for Factor
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.codewars.com/kata/55cb854deb36f11f130000e1"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Grasshopper - Debug
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.codewars.com/kata/56a29b237e9e997ff2000048"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Grasshopper - Create the Rooms
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.codewars.com/kata/56c5847f27be2c3db20009c3"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Subtract the Sum
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.codewars.com/kata/514a7ac1a33775cbb500001e"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Return to Sanity
+            </a>
+            <a
+              href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Working_with_Objects"
+              target="_blank"
+              rel="noreferrer"
+            >
+              - HINT
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.codewars.com/kata/514a3996d22ce03198000003"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Mr. Freeze
+            </a>
+            <a
+              href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/freeze"
+              target="_blank"
+              rel="noreferrer"
+            >
+              - HINT
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.codewars.com/kata/514aa0dc21607ae236000017"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Shifty Closures
+            </a>
+            <a
+              href="https://developer.mozilla.org/en-US/docs/Glossary/Scope"
+              target="_blank"
+              rel="noreferrer"
+            >
+              - HINT
+            </a>
+          </li>
+        </ol>
+      </div>
     </div>
   );
 }
